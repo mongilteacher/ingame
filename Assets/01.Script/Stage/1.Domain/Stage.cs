eb3c4d07fa471588a5b9fe4635af49f4e8de3650
mongilteacher <mongilteacher@gmail.com>
2025-06-17 10:10:42 +0900

    StageLevel

---
+++ Assets/01.Script/Stage/1.Domain/Stage.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Stage
+{
+    public int LevelNumber { get; private set; }
+    
+    private StageLevel _currentLevel;
+    private float _progressTime;
+    
+    public List<StageLevel> Levels { get; private set; } = new List<StageLevel>();
+
+
+    public Stage(int levelNumber, float progressTime)
+    {
+        if (levelNumber < 0)
+        {
+            throw new Exception("올바르지 않은 레벨넘버 입니다.");
+        }
+
+        if (progressTime < 0)
+        {
+            throw new Exception("올바르지 않은 진행 시간입니다.");
+        }
+        
+        
+        LevelNumber = LevelNumber;
+        _progressTime = _progressTime;
+    }
+    
+    public void AddLevel(StageLevel level)
+    {
+        if (level == null)
+        {
+            throw new Exception("레벨이 null입니다.");
+        }
+        
+        Levels.Add(level);
+    }
+
+    public void Progress(float dt)
+    {
+        _progressTime += dt;
+
+        if (_currentLevel.TryLevelUp(_progressTime))
+        {
+            _progressTime = 0;
+            
+            if (_currentLevel.IsClear())
+            {
+                LevelNumber += 1;
+                _currentLevel = Levels[LevelNumber - 1];
+            }
+        }
+    }
+}
---
+++ Assets/01.Script/Stage/1.Domain/StageLevel.cs
@@ -0,0 +1,84 @@
+using System;
+using UnityEngine.ProBuilder.Shapes;
+
+public class StageLevel 
+{
+    // 기획 데이터
+    public readonly int StartLevel;             // 시작 레벨
+    public readonly int EndLevel;               // 종료 레벨
+    public float HealthFactor;                  // 체력 배율
+    public float DamageFactor;                  // 대미지 배율
+    public float Duration => 60f;          // 기준 시간
+    public readonly float SpawnInterval;        // 스폰 주기
+    public readonly float SpawnRate;            // 스폰 확률
+
+    // 상태 데이터             
+    public int CurrentLevel; // StartLevel ~ EndLevel
+
+    public StageLevel(int startLevel, int endLevel, float healthFactor, float damageFactor, float spawnInterval, float spawnRate , int currentLevel)
+    {
+        if (startLevel < 0 || endLevel <= startLevel)
+        {
+            throw new Exception("시작 레벨이 올바르지 않습니다.");
+        }
+        if (endLevel < 0 || endLevel <= startLevel)
+        {
+            throw new Exception("종료 레벨이 올바르지 않습니다.");
+        }
+
+        if (healthFactor < 1)
+        {
+            throw new Exception("체력 배율이 올바르지 않습니다.");
+        }
+        if (damageFactor < 1)
+        {
+            throw new Exception("공격력 배율이 올바르지 않습니다.");
+        }
+
+        if (spawnInterval <= 0)
+        {
+            throw new Exception("스폰 주기가 올바르지 않습니다.");
+        }
+        
+        if (spawnRate <= 0 || 100 < spawnRate)
+        {
+            throw new Exception("스폰 확률이 올바르지 않습니다.");
+        }
+
+        if (currentLevel < startLevel || endLevel < currentLevel)
+        {
+            throw new Exception("현재 레벨이 올바르지 않습니다.");
+        }
+        
+        StartLevel = startLevel;
+        EndLevel = endLevel;
+        HealthFactor = healthFactor;
+        DamageFactor = damageFactor;
+        SpawnInterval = spawnInterval;
+        SpawnRate = spawnRate;
+        CurrentLevel = currentLevel;
+    }
+
+
+    public bool TryLevelUp(float progressTime)
+    {/*
+        if (CurrentLevel >= EndLevel)
+        {
+            return false;
+        }
+        */
+        
+        if (progressTime >= Duration)
+        {
+            CurrentLevel += 1;
+            return true;
+        }
+
+        return false;
+    }
+
+    public bool IsClear()
+    {
+        return CurrentLevel == EndLevel;
+    }
+}
