d8f5a1b22dd42a48cf4dd0eb174ad1375ba50bc0
mongilteacher <mongilteacher@gmail.com>
2025-06-18 14:43:44 +0900

    1. 파이어베이스 연동

---
+++ Assets/01.Script/Infrastructure/FirebaseTest.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using Firebase;
+using Firebase.Extensions;
+
+public class FirebaseTest : MonoBehaviour
+{
+    private FirebaseApp _app;
+    
+    private void Start()
+    {
+        Init();
+    }
+
+
+    // 파이어베이스 내 프로젝트에 연결
+    private void Init()
+    {
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task => {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available) 
+            {
+                Debug.Log("파이어베이스 연결에 성공했습니다.");
+                _app = Firebase.FirebaseApp.DefaultInstance;
+
+            } else 
+            {
+                Debug.LogError($"파이어베이스 연결 실패했습니다. ${dependencyStatus}");
+            }
+        });
+    }
+    
+}
