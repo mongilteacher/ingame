c025ffc6be2709db0132a5274cf740150f8ff8e0
mongilteacher <mongilteacher@gmail.com>
2025-06-18 16:08:25 +0900

    데이터 저장/조회

--- Assets/01.Script/Infrastructure/FirebaseTest.cs
+++ Assets/01.Script/Infrastructure/FirebaseTest.cs
@@ -1,13 +1,15 @@
-using System;
+using System.Collections.Generic;
 using Firebase;
 using Firebase.Auth;
 using Firebase.Extensions;
+using Firebase.Firestore;
 using UnityEngine;
 
 public class FirebaseTest : MonoBehaviour
 {
     private FirebaseApp _app;
     private FirebaseAuth _auth;
+    private FirebaseFirestore _db;
 
     private void Start()
     {
@@ -24,8 +26,10 @@ public class FirebaseTest : MonoBehaviour
             if (dependencyStatus == DependencyStatus.Available)
             {
                 Debug.Log("파이어베이스 연결에 성공했습니다.");
+
                 _app = FirebaseApp.DefaultInstance;
                 _auth = FirebaseAuth.DefaultInstance;
+                _db = FirebaseFirestore.DefaultInstance;
 
                 Login();
             }
@@ -70,8 +74,11 @@ public class FirebaseTest : MonoBehaviour
 
             var result = task.Result;
             Debug.LogFormat("로그인에 성공했습니다: {0} ({1})", result.User.DisplayName, result.User.UserId);
-            
+
             NicknameChange();
+
+            AddMyRanking();
+            GetMyRanking();
         });
     }
 
@@ -80,12 +87,12 @@ public class FirebaseTest : MonoBehaviour
         var user = _auth.CurrentUser;
 
         if (user == null) return;
-        
+
         var profile = new UserProfile
         {
-            DisplayName = "teemo",
+            DisplayName = "teemo"
         };
-        
+
         user.UpdateUserProfileAsync(profile).ContinueWithOnMainThread(task =>
         {
             if (task.IsCanceled || task.IsFaulted)
@@ -93,19 +100,72 @@ public class FirebaseTest : MonoBehaviour
                 Debug.LogError($"닉네임 변경에 실패했습니다: {task.Exception.Message}");
                 return;
             }
-            
+
             Debug.Log("닉네임 변경에 성공했습니다.");
         });
     }
 
     private void GetProfile()
     {
-        Firebase.Auth.FirebaseUser user = _auth.CurrentUser;
+        var user = _auth.CurrentUser;
         if (user == null) return;
-        
-        string nickname = user.DisplayName;
-        string email = user.Email;
 
-        Account account = new Account(email, nickname, "firebase");
+        var nickname = user.DisplayName;
+        var email = user.Email;
+
+        var account = new Account(email, nickname, "firebase");
+    }
+
+
+    private void AddMyRanking()
+    {
+        var ranking = new Ranking("huhuhu@gmail.com", "허허허", 7900);
+
+        var rankingDict = new Dictionary<string, object>
+        {
+            { "Email", ranking.Email },
+            { "Nickname", ranking.Nickname },
+            { "Score", ranking.Score }
+        };
+
+
+        _db.Collection("rankings").AddAsync(rankingDict).ContinueWithOnMainThread(task =>
+        {
+            if (task.IsCanceled || task.IsFaulted)
+            {
+                Debug.LogError($"데이터 추가가 실패했습니다.: {task.Exception.Message}");
+                return;
+            }
+        });
+        
+        _db.Collection("rankings").Document(ranking.Email).SetAsync(rankingDict).ContinueWithOnMainThread(task =>
+        {
+            Debug.Log(string.Format("Added or Updated document with ID: {0}.", task.Id));
+        });
+    }
+
+    private void GetMyRanking()
+    {
+        var email = "huhuhu@gmail.com"; // ID 역할
+
+        var docRef = _db.Collection("rankings").Document(email);
+        docRef.GetSnapshotAsync().ContinueWithOnMainThread(task =>
+        {
+            var snapshot = task.Result;
+            
+            if (snapshot.Exists)
+            {
+                Debug.Log(string.Format("Document data for {0} document:", snapshot.Id));
+                var rankingDict = snapshot.ToDictionary();
+                foreach (var pair in rankingDict)
+                {
+                    Debug.Log(string.Format("{0}: {1}", pair.Key, pair.Value));
+                }
+            }
+            else
+            {
+                Debug.Log(string.Format("Document {0} does not exist!", snapshot.Id));
+            }
+        });
     }
 }
